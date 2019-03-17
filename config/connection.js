834a2f6997f5be894ffc78f38c5491cfebe9edbe
fenice <danielhan@Daniels-MacBook-Air.local>
2019-03-16 22:23:26 -0400

    .

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const config_localhost = {
     "host"    : "localhost",
     "port"    : 3306,
     "user"    : "root",
-    "password": "",
+    "password": "password",
     "database": "burgers_db"
 };
 
